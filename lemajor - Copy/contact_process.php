b50f171b1310bc692dd5796a64017cf98767b374
rCodeAlpha3 <65119774+rCodeAlpha3@users.noreply.github.com>
2020-05-20 14:03:38 +0200

    Update contact_process.php

--- lemajor - Copy/contact_process.php
+++ lemajor - Copy/contact_process.php
@@ -1,45 +1,88 @@
 <?php
+require "PHPMailer/PHPMailerAutoload.php";
 
-	$to = "root96void@gmail.com";
-	if(isset($_POST['submit']))
-	{
-    $from = $_REQUEST['email'];
-    $name = $_REQUEST['name'];
-    $subject = $_REQUEST['subject'];
-    $number = $_REQUEST['number'];
-    $cmessage = $_REQUEST['message'];
+function smtpmailer($to, $from, $from_name, $subject, $body)
+    {
+      
+        $mail = new PHPMailer();
+        $mail->IsSMTP();
+        $mail->SMTPAuth = true; 
+ 
+        $mail->SMTPSecure = 'ssl'; 
+        $mail->Host = 'mail.lemajorgroup.com';
+        $mail->Port = 465;  
+        $mail->Username = 'admin@lemajorgroup.com';
+        $mail->Password = 'Admin_pass01';   
+   
+   //   $path = 'reseller.pdf';
+   //   $mail->AddAttachment($path);
+   
+        $mail->IsHTML(true);
+        $mail->From="admin@lemajorgroup.com";
+        $mail->FromName=$from_name;
+        $mail->Sender=$from;
+        $mail->AddReplyTo($from, $from_name);
+        $mail->Subject = $subject;
+        $mail->Body = $body;
+        $mail->AddAddress($to);
+        if(!$mail->Send())
+        {
+            $error ="Please try Later, Error Occured while Processing...";
+            return $error; 
+        }
+        else 
+        {
+            $error = "Thanks You !! Your email is sent.";  
+            return $error;
+        }
+    }
+    
+    if(isset($_POST['submit'])){
+        $secretKey = '6Le8EfoUAAAAAJEDZQtZ2vHsvmH4dtRSPh_rpPWp';
+        $captcha = $_POST['g-recaptcha-response'];
 
-    $headers = "From: $from";
-	$headers = "From: " . $from . "\r\n";
-	$headers .= "Reply-To: ". $from . "\r\n";
-	$headers .= "MIME-Version: 1.0\r\n";
-	$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+        if(!$captcha){
+          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
+          exit;
+        }
 
-    $subject = "You have a message from your Bitmap Photography.";
 
-    $logo = 'img/logo.png';
-    $link = '#';
 
-	$body = "<!DOCTYPE html><html lang='en'><head><meta charset='UTF-8'><title>Express Mail</title></head><body>";
-	$body .= "<table style='width: 100%;'>";
-	$body .= "<thead style='text-align: center;'><tr><td style='border:none;' colspan='2'>";
-	$body .= "<a href='{$link}'><img src='{$logo}' alt=''></a><br><br>";
-	$body .= "</td></tr></thead><tbody><tr>";
-	$body .= "<td style='border:none;'><strong>Name:</strong> {$name}</td>";
-	$body .= "<td style='border:none;'><strong>Email:</strong> {$from}</td>";
-	$body .= "</tr>";
-	$body .= "<tr><td style='border:none;'><strong>Subject:</strong> {$csubject}</td></tr>";
-	$body .= "<tr><td></td></tr>";
-	$body .= "<tr><td colspan='2' style='border:none;'>{$cmessage}</td></tr>";
-	$body .= "</tbody></table>";
-	$body .= "</body></html>";
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $response=file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=".$secretKey."&response=".$captcha."&remoteip=".$ip);
+        $responseKeys = json_decode($response,true);
 
-    $send = mail($to, $subject, $body, $headers);
-if($send){
-?><script>alert('Successful');</script> <?php
+        if(intval($responseKeys["success"]) !== 1) {
+          echo '<p class="alert alert-warning">Please check the the captcha form.</p>';
+        } else {
+
+
+    $mail_to = "admin@lemajorgroup.com";
+        
+    # Sender Data
+    $subject = trim($_POST["subject"]);
+    $name = str_replace(array("\r","\n"),array(" "," ") , strip_tags(trim($_POST["name"])));
+    $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
+    $message = trim($_POST["message"]);
+    
+    if ( empty($name) OR !filter_var($email, FILTER_VALIDATE_EMAIL) OR empty($subject) OR empty($message)) {
+        # Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo '<p class="alert alert-warning">Please complete the form and try again.</p>';
+        exit;
+    }
+  
+    $error=smtpmailer($mail_to,$email, $name ,$subject, $message);
 }
-else{
-	die('error');
-
-	}}
+    }
 ?>
+
+<html>
+    <head>
+        <title>PHPMailer 5.2 testing from DomainRacer</title>
+    </head>
+    <body style="background: black;">
+        <center><h2 style="padding-top:70px;color: white;"><?php echo $error; ?></h2></center>
+    </body>
+    
+</html>
